3f22c74559a6ccca3994d01c595e30a06b332162
eberry <>
2009-05-29 21:42:11 +0000

    Edited for use with CaloTowerComp analyzer

--- Producers/CaloTowersFromTrigPrimsCreator/calotowersfromtrigprimscreator_cfg.py
+++ Producers/CaloTowersFromTrigPrimsCreator/calotowersfromtrigprimscreator_cfg.py
@@ -4,21 +4,29 @@ process = cms.Process("MyCaloTowerCreator")
 
 process.load("FWCore.MessageService.MessageLogger_cfi")
 
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(10) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100) )
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring(
+        'file:/uscms/home/eberry/data/qcdDijets_ReRunL1_withCMSSW_2_2_3_ptBin9_jetThreshold20GeV_job1.root'
+    )
+)
 
+## Ordinary cff files
 process.load("Configuration.StandardSequences.Services_cff")
 process.load("Configuration.StandardSequences.Geometry_cff")
 process.load("Configuration.StandardSequences.MagneticField_cff")
 process.load("Configuration.StandardSequences.FakeConditions_cff")
 process.load("Configuration.StandardSequences.RawToDigi_cff")
+process.load("Configuration.StandardSequences.Reconstruction_cff")
 
+## My CaloTower producer
 process.load("Producers.CaloTowersFromTrigPrimsCreator.calotowersfromtrigprimscreator_cfi")
 
-process.source = cms.Source("PoolSource",
-    fileNames = cms.untracked.vstring(
-        'file:/uscms/home/eberry/data/test.root'
-    )
-)
+## My CaloTower comparator 
+process.load("Analyzers.CaloTowerComp.calotowercomp_cfi")
+
+## The default CaloTower producer
+process.load("RecoJets.JetProducers.CaloTowerSchemeB_cfi")
 
 process.out = cms.OutputModule("PoolOutputModule",
     fileName = cms.untracked.string('data/final_output.root'),
@@ -30,13 +38,18 @@ process.out = cms.OutputModule("PoolOutputModule",
         )
 )
 
-process.unpack = cms.Path(process.RawToDigi)
-process.produce = cms.Path(process.caloTowersFromTrigPrimsCreator)
-process.finalProcess = cms.EndPath(process.out)
+process.calolocalreco_noEcalPreshower = cms.Sequence(process.ecalLocalRecoSequence_nopreshower+process.hcalLocalRecoSequence)
+
+process.Unpack      = cms.Path(process.RawToDigi)
+process.Reconstruct = cms.Path(process.calolocalreco_noEcalPreshower)
+process.Produce     = cms.Path(process.towerMaker + process.caloTowersFromTrigPrimsCreator)
+process.Analyze     = cms.Path(process.caloTowerComparator)
+process.SaveAll     = cms.EndPath(process.out)
 
 process.schedule = cms.Schedule()
-##process.schedule.append(process.unpack)
-process.schedule.append(process.produce)
-process.schedule.append(process.finalProcess)
-
+process.schedule.append(process.Unpack)
+process.schedule.append(process.Reconstruct)
+process.schedule.append(process.Produce)
+process.schedule.append(process.Analyze)
+## process.schedule.append(process.SaveAll)
 
