67a9d6bb48294510db59c8731aeb09aaf8fc8b21
eberry <>
2009-07-17 11:52:51 +0000

    Added verbosity argument

--- Producers/CaloTowersFromTrigPrimsCreator/python/calotowersfromtrigprimsanalyzer_cfi.py
+++ Producers/CaloTowersFromTrigPrimsCreator/python/calotowersfromtrigprimsanalyzer_cfi.py
@@ -4,5 +4,6 @@ caloTowersFromTrigPrimsAnalyzer = cms.EDAnalyzer('CaloTowersFromTrigPrimsAnalyze
                                                  caloTowerTag = cms.untracked.InputTag("caloTowersFromTrigPrimsCreator"),
                                                  hcalTrigPrimTag = cms.untracked.InputTag("simHcalTriggerPrimitiveDigis"),
                                                  ecalTrigPrimTag = cms.untracked.InputTag("simEcalTriggerPrimitiveDigis"),
-                                                 outputFileName = cms.untracked.string("CaloTowersFromTrigPrimsAnalyzer.root")
+                                                 outputFileName = cms.untracked.string("CaloTowersFromTrigPrimsAnalyzer.root"),
+                                                 verbose = cms.untracked.bool(False)
                                                  )
