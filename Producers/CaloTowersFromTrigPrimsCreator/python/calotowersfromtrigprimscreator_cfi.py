6a45be6f8562db04bb450cbd43c73d02df6a9519
eberry <>
2009-07-17 11:37:10 +0000

    Removed momentum depth information.  Removed 'defaultCaloTowersTag'.

--- Producers/CaloTowersFromTrigPrimsCreator/python/calotowersfromtrigprimscreator_cfi.py
+++ Producers/CaloTowersFromTrigPrimsCreator/python/calotowersfromtrigprimscreator_cfi.py
@@ -3,18 +3,11 @@ import FWCore.ParameterSet.Config as cms
 from Geometry.CaloEventSetup.EcalTrigTowerConstituents_cfi import *
 from RecoJets.Configuration.CaloTowersES_cfi import *
 
-caloTowersFromTrigPrimsCreator = cms.EDProducer('CaloTowersFromTrigPrimsCreator',
-                                                
-  momHBDepth = cms.untracked.double(0.2),
-  momHEDepth = cms.untracked.double(0.4),
-  momEBDepth = cms.untracked.double(0.3),
-  momEEDepth = cms.untracked.double(0.0),   
+caloTowersFromTrigPrimsCreator = cms.EDProducer('CaloTowersFromTrigPrimsCreator',                                                
   hadThreshold = cms.untracked.double(0.0),                                                
   emThreshold  = cms.untracked.double(0.0),                                                
   hcalTrigPrimTag = cms.untracked.InputTag("simHcalTriggerPrimitiveDigis"),
   ecalTrigPrimTag = cms.untracked.InputTag("simEcalTriggerPrimitiveDigis"),
-  defaultCaloTowersTag = cms.untracked.InputTag("towerMaker"),
   useHF = cms.untracked.bool(True),                                               
-  verbose = cms.untracked.bool(False)
-                                                
+  verbose = cms.untracked.bool(False)                                                
 )
