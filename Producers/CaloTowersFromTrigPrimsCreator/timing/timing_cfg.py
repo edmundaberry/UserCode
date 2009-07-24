76238e597439dbf65a635ff01dc3d7e90432f3b3
eberry <>
2009-07-24 12:04:27 +0000

    Timing study cfg.py file and folder

---
+++ Producers/CaloTowersFromTrigPrimsCreator/timing/timing_cfg.py
@@ -0,0 +1,44 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("HLT2")
+
+# Message logging
+process.load("FWCore.MessageService.MessageLogger_cfi")
+process.options = cms.untracked.PSet(
+    wantSummary = cms.untracked.bool(True)
+)
+
+# Load source
+process.load("Producers.CaloTowersFromTrigPrimsCreator.RelValQCD_LowPt_cfi")
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(100)
+)
+
+# Load geometry/detector info
+process.load("Configuration.StandardSequences.GeometryPilot2_cff")
+process.load("Configuration.StandardSequences.MagneticField_cff")
+
+# Timing service
+process.PathTimerService = cms.Service( "PathTimerService" )
+process.hltTimer = cms.EDProducer( "PathTimerInserter" )
+
+# Conditions
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+process.GlobalTag.globaltag = 'IDEAL_V12::All'
+
+# Setup HLT for running my mini menu
+process.load("SimGeneral.HepPDTESSource.pythiapdt_cfi")
+process.load("Producers.CaloTowersFromTrigPrimsCreator.MyMiniHLTMenu_cff")
+process.schedule = process.MyHLTSchedule 
+
+# Setup output
+process.load("Configuration.EventContent.EventContent_cff")
+process.hltPoolOutput = cms.OutputModule("PoolOutputModule",
+                                         outputCommands = cms.untracked.vstring( 'drop *', 'keep HLTPerformanceInfo_*_*_*' ),
+                                         fileName = cms.untracked.string('file:HLT.root')
+)
+
+process.HLTOutput = cms.EndPath(process.hltTimer + process.hltPoolOutput)
+
+# Add output to the schedule
+process.schedule.append(process.HLTOutput)
