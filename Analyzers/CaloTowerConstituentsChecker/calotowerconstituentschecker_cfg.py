8cd14e0d22227be776f941c8dae2ba3987c740ca
eberry <>
2009-07-28 12:24:10 +0000

    Added flags for checking double counting in HCAL

--- Analyzers/CaloTowerConstituentsChecker/calotowerconstituentschecker_cfg.py
+++ Analyzers/CaloTowerConstituentsChecker/calotowerconstituentschecker_cfg.py
@@ -13,7 +13,11 @@ process.source = cms.Source("EmptySource")
 process.demo = cms.EDAnalyzer('CaloTowerConstituentsChecker',
                               verbose              = cms.untracked.bool(True),
                               check_eb_doublecount = cms.untracked.bool(True),
-                              check_ee_doublecount = cms.untracked.bool(True)
+                              check_ee_doublecount = cms.untracked.bool(True),
+                              check_hb_doublecount = cms.untracked.bool(True),
+                              check_he_doublecount = cms.untracked.bool(True),
+                              check_ho_doublecount = cms.untracked.bool(True),
+                              check_hf_doublecount = cms.untracked.bool(True)
 )
 
 process.p = cms.Path(process.demo)
