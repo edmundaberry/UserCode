79dd555c7464dcbc7b1d011c5c0648ff3600693c
eberry <>
2009-08-05 19:01:50 +0000

    In case we want to use non-sorted CaloTower collections

---
+++ Producers/CaloTowersFromTrigPrimsCreator/interface/CaloTowerNonSortedCollection.h
@@ -0,0 +1,9 @@
+#ifndef CALOTOWERNONSORTEDCOLLECTION_H
+#define CALOTOWERNONSORTEDCOLLECTION_H
+
+#include "DataFormats/Common/interface/EDCollection.h"
+#include "DataFormats/CaloTowers/interface/CaloTower.h"
+
+typedef edm::EDCollection<CaloTower> CaloTowerNonSortedCollection;
+
+#endif
