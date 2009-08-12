da6cb8b9b9f2303a0eab3fc877e6369ff43e6404
eberry <>
2009-08-12 16:48:00 +0000

    Updated with proper tags

--- Producers/HcalSLHCTrigPrimDigiProducer/python/hcalSlhcTpDigi_cfi.py
+++ Producers/HcalSLHCTrigPrimDigiProducer/python/hcalSlhcTpDigi_cfi.py
@@ -1,4 +1,19 @@
 import FWCore.ParameterSet.Config as cms
 
-simHcalSLHCTriggerPrimitiveDigis = cms.EDProducer('HcalSLHCTrigPrimDigiProducer'
+
+from CalibCalorimetry.CaloTPG.CaloTPGTranscoder_cfi import *
+HcalTPGCoderULUT = cms.ESProducer("HcalTPGCoderULUT",
+    read_Ascii_LUTs = cms.bool(False),
+    read_XML_LUTs = cms.bool(False),
+    LUTGenerationMode = cms.bool(True),
+    DumpL1TriggerObjects = cms.bool(False),
+    TagName = cms.string("LUTFromHCALDb"),
+    AlgoName = cms.string("LUTFromHCALDb"),
+    inputLUTs = cms.FileInPath('CalibCalorimetry/HcalTPGAlgos/data/inputLUTcoder_physics.dat'),
+    filename = cms.FileInPath('CalibCalorimetry/HcalTPGAlgos/data/RecHit-TPG-calib.dat')
+)
+
+simHcalSLHCTriggerPrimitiveDigis = cms.EDProducer('HcalSLHCTrigPrimDigiProducer',
+  hbheDigis = cms.InputTag("simHcalDigis"),
+  hfDigis   = cms.InputTag("simHcalDigis")                                               
 )
