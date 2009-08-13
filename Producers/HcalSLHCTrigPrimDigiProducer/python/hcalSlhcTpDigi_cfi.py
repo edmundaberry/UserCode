cc4504d77a52dd03c8b3c942fe88ceb27c73720b
eberry <>
2009-08-13 16:13:42 +0000

    Added values to correspond to default python cfg file

--- Producers/HcalSLHCTrigPrimDigiProducer/python/hcalSlhcTpDigi_cfi.py
+++ Producers/HcalSLHCTrigPrimDigiProducer/python/hcalSlhcTpDigi_cfi.py
@@ -15,5 +15,14 @@ HcalTPGCoderULUT = cms.ESProducer("HcalTPGCoderULUT",
 
 simHcalSLHCTriggerPrimitiveDigis = cms.EDProducer('HcalSLHCTrigPrimDigiProducer',
   hbheDigis = cms.InputTag("simHcalDigis"),
-  hfDigis   = cms.InputTag("simHcalDigis")                                               
+  hfDigis   = cms.InputTag("simHcalDigis"),
+  latency = cms.int32(1),
+  weights = cms.vdouble(1.0, 1.0), ##hardware algo 
+  peakFilter = cms.bool(True),
+  firstTPSample = cms.int32(2),
+  TPSize = cms.int32(4),
+  FG_threshold = cms.uint32(12),   ## threshold for setting fine grain bit
+  ZS_threshold = cms.uint32(1),     ## threshold for setting fine grain bit
+  minIsoDepth = cms.int32(0),
+  maxIsoDepth = cms.int32(20)
 )
