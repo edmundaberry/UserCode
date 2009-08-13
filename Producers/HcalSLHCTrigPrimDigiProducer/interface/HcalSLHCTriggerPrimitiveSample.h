a66ed0454fa4372869cd0d7a1f492edad81cd6e5
eberry <>
2009-08-13 16:11:15 +0000

    Neatness update

--- Producers/HcalSLHCTrigPrimDigiProducer/interface/HcalSLHCTriggerPrimitiveSample.h
+++ Producers/HcalSLHCTrigPrimDigiProducer/interface/HcalSLHCTriggerPrimitiveSample.h
@@ -10,17 +10,15 @@ class HcalSLHCTriggerPrimitiveSample {
   
   HcalSLHCTriggerPrimitiveSample();
   HcalSLHCTriggerPrimitiveSample(uint32_t data);
-  HcalSLHCTriggerPrimitiveSample(int encodedEt, int encodedIsoEt, int fineGrain, 
-				 int slb, int slbchan);
+  HcalSLHCTriggerPrimitiveSample(int encodedEt, int encodedIsoEt, int fineGrain, int slb, int slbchan);
   
-  uint32_t raw() const {return theSample; }
-  
-  int compressedEt   () const { return ((theSample >> 0 ) & 0xFF); }
-  int compressedIsoEt() const { return ((theSample >> 8 ) & 0xFF); }
-  int fineGrain      () const { return ((theSample >> 16) & 0xFF); }
-  int slbChan        () const { return ((theSample >> 27) & 0x3 ); }
-  int slb            () const { return ((theSample >> 29) & 0x7 ); }
+  uint32_t raw() const {return theSample; }  
+  int slb            () const { return ((theSample >> 29) & 0x7 ); } // slb          has 3 bits, (30-32)
+  int slbChan        () const { return ((theSample >> 27) & 0x3 ); } // slbchan      has 2 bits, (28-29)
   int slbAndChan     () const { return ((theSample >> 27) & 0x1F); }
+  int fineGrain      () const { return ((theSample >> 16) & 0xFF); } // fine grain   has 8 bits, (17-24)
+  int compressedIsoEt() const { return ((theSample >> 8 ) & 0xFF); } // isolation et has 8 bits, (9 -16)
+  int compressedEt   () const { return ((theSample >> 0 ) & 0xFF); } // et           has 8 bits, (1 -8 )
   
  private:
   uint32_t theSample;
